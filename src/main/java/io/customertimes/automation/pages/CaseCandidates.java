bf4dc098b7f5fb66d8c34c2f98ce1ff76ac0ea87
dmytrolatyshev <d_latyshev@ukr.net>
2018-12-28 11:59:13 +0200

    Refactoring

---
+++ src/main/java/io/customertimes/automation/pages/CaseCandidates.java
@@ -0,0 +1,10 @@
+package io.customertimes.automation.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CaseCandidates {
+
+      private SelenideElement title = $("article[data-aura-class = \"cCaseCandidatesList\"] h2 > span");
+
+}
