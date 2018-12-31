a864ed798edf66936fbffc65aa3f4a259b45564e
Roman Tyshchenko <>
2018-12-31 15:12:52 +0200

    refactoring

--- src/main/java/io/customertimes/automation/pages/HomePage.java
+++ src/main/java/io/customertimes/automation/pages/HomePage.java
@@ -1,18 +1,20 @@
 package io.customertimes.automation.pages;
 
 import com.codeborne.selenide.ElementsCollection;
+
 import static com.codeborne.selenide.Selenide.$$;
 
 public class HomePage extends Base {
 
     private CaseCandidates caseCandidates;
+
     private ElementsCollection listOfSourceDocuments = $$("th[data-label=\"Account Name\"] a");
 
     public HomePage() {
         this.isPageOpened = pageIsOpened();
     }
 
-    public void selectSourceDocumentFromTheList(int index){
+    public void selectSourceDocumentFromTheList(int index) {
         listOfSourceDocuments.get(index).click();
     }
 
