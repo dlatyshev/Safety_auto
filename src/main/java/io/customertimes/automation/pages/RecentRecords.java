2fbfeeab0f35d36529597814db4e450083b25710
Roman Tyshchenko <>
2019-01-04 14:25:02 +0200

    create temporary solution

--- src/main/java/io/customertimes/automation/pages/RecentRecords.java
+++ src/main/java/io/customertimes/automation/pages/RecentRecords.java
@@ -1,6 +1,7 @@
 package io.customertimes.automation.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -14,5 +15,11 @@ public class RecentRecords extends Base {
 
     public boolean isOpened() {
         return isPageOpened;
+    }
+
+    // temporary solution, change after moving to dev org.
+    public CasePage clickOnCase() {
+        $(By.xpath("//a[text()=0001220]")).click();
+        return new CasePage();
     }
 }
