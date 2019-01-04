f2ef37081493121ec883dc37379bda4fa7ba0c5c
Roman Tyshchenko <>
2019-01-04 14:24:38 +0200

    implement step files

--- src/test/java/io/customertimes/automation/steps/CaseSteps.java
+++ src/test/java/io/customertimes/automation/steps/CaseSteps.java
@@ -1,21 +1,43 @@
 package io.customertimes.automation.steps;
 
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import io.customertimes.automation.pages.CasePage;
+import io.customertimes.automation.pages.HomePage;
 import io.customertimes.automation.utilities.TestUtils;
-import org.testng.Assert;
 
 public class CaseSteps extends TestUtils {
 
     private CasePage casePage = new CasePage();
+    private HomePage homePage = new HomePage();
 
-    @Then("^case details are opened$")
-    public void caseDetailsAreOpened()  {
-        Assert.assertTrue(casePage.isOpened());
+    @When("^user clicks resent opened case$")
+    public void user_clicks_resent_opened_case() {
+        homePage.selectRecentRecords().clickOnCase();
     }
 
-    @Then("^user can edit case information manually$")
-    public void userCanEditCaseInformationManually()  {
-        // stub
+    @Then("^Case navigation is present$")
+    public void case_navigation_is_present() {
+        casePage.selectCaseNavigation().isOpened();
+    }
+
+    @Then("^Milestones is present$")
+    public void milestones_is_present() {
+        casePage.selectMilestones().isOpened();
+    }
+
+    @Then("^Possible duplication is present$")
+    public void possible_duplication_is_present() {
+        casePage.selectPossibleDuplicates().isOpened();
+    }
+
+    @Then("^Header bunner is present$")
+    public void header_bunner_is_present() {
+        casePage.selectHeaderBunner().isOpened();
+    }
+
+    @Then("^Source document is present$")
+    public void source_document_is_present() {
+        casePage.selectSourceDocumentPage().isOpened();
     }
 }
