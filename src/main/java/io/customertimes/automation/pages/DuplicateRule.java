fe5f6ccf41de942b1e516de5c0dd30d3c81d9c33
dmytrolatyshev <d_latyshev@ukr.net>
2019-01-09 16:10:33 +0200

    Fix bugs

--- src/main/java/io/customertimes/automation/pages/DuplicateRule.java
+++ src/main/java/io/customertimes/automation/pages/DuplicateRule.java
@@ -3,10 +3,11 @@ package io.customertimes.automation.pages;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
+import static org.openqa.selenium.By.xpath;
 
 public class DuplicateRule extends DuplicateRulesPage {
 
-    private SelenideElement duplicateRuleHeader = $("//span[text() = \"Duplicate Rule\"]");
+    private SelenideElement duplicateRuleHeader = $(xpath("//span[text() = \"Duplicate Rule\"]"));
 
     public DuplicateRule() {
         isOpened = pageIsOpened(duplicateRuleHeader);
