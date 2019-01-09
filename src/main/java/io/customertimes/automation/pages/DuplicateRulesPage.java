ded779c474f3a34af5aef5d44118843d3962c1f3
dmytrolatyshev <d_latyshev@ukr.net>
2019-01-09 11:19:45 +0200

    Create a new class "DuplicateRulesSteps"

--- src/main/java/io/customertimes/automation/pages/DuplicateRulesPage.java
+++ src/main/java/io/customertimes/automation/pages/DuplicateRulesPage.java
@@ -1,5 +1,14 @@
 package io.customertimes.automation.pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class DuplicateRulesPage extends Base {
 
+    private SelenideElement duplicateRulesListHeader = $("span[title = \"Duplicate Rule Name\"]");
+
+    public DuplicateRulesPage() {
+        this.isPageOpened = pageIsOpened(duplicateRulesListHeader);
+    }
 }
